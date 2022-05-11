d10e802b12fcebcf9cfdac05585f71abd3f3ac14
fihadranzer <rhfiha@gmail.com>
2022-05-11 09:35:57 +0600

    SecondEdition

--- script.js
+++ script.js
@@ -3,13 +3,27 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const quoteBtn = document.getElementById("quote-btn");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
+// Loader
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 //Random function
 
 //******Local QUOTES Here*******//
 function newQuote() {
+  loading();
+
   let quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
   if (!quote.author) {
@@ -27,12 +41,14 @@ function newQuote() {
   }
 
   quoteText.textContent = quote.text;
+  complete();
 }
 //******Local QUOTES Here*******//
 
 //Get Quotes from API
 
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
@@ -41,6 +57,7 @@ async function getQuotes() {
   } catch (error) {
     console.log(error);
   }
+  
 }
 
 // Twitter post
@@ -55,6 +72,6 @@ twitterBtn.addEventListener("click", tweetQuote);
 
 //-------on load---------//
 
-//getQuotes();
+getQuotes();
 
 //-------on load---------//
